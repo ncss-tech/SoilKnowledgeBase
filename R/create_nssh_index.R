32475fcd8c663e0a8682e7ec74cf0c9f822e5cbd
Andrew G. Brown <andrew.g.brown@ca.usda.gov>
2020-11-30 13:19:50 -0800

    consolidate NSSH parsing code

--- R/create_nssh_index.R
+++ R/create_nssh_index.R
@@ -30,7 +30,7 @@ create_nssh_index <- function(
   keep_pdf = FALSE
 ) {
 
-  if(is.null(url))
+  if (is.null(url))
     url <- "https://www.nrcs.usda.gov/wps/portal/nrcs/detail/soils/ref/?cid=nrcs142p2_054240"
 
   ## NSSH Table of Contents
@@ -139,6 +139,43 @@ create_nssh_index <- function(
   return(res)
 }
 
+#' Parse headers and line positions by NSSH Part and Subpart
+#'
+#' @param number Vector of part number(s) e.g. \code{600:614}
+#' @param subpart Vector of subpart characters e.g. \code{"A"}
+#'
+#' @return A data.frame containing line numbers corresponding to NSSH part and subpart headers.
+#' @export
+parse_nssh_part <- function(number, subpart) {
+  
+  do.call('rbind', lapply(split(data.frame(number = number, subpart = subpart),
+                                1:length(number)), function(x) {
+                                  
+                                  idx <- respart <- ressubpart <- numeric(0)
+                                  
+                                  try( {
+                                    f <- sprintf("inst/extdata/NSSH/%s/%s%s.txt",
+                                                 x$number, x$number, x$subpart)
+                                    
+                                    if(!file.exists(f))
+                                      return(NULL)
+                                    
+                                    L <- readLines(f, warn = FALSE)
+                                    
+                                    idx <- grep("^\\d{3}\\.\\d+ [A-Z]", L)
+                                    
+                                    respart <- rep(x$number, length(idx))
+                                    ressubpart <- rep(x$subpart, length(idx))
+                                    
+                                    
+                                    res <- data.frame(part = x$number,
+                                                      subpart = x$subpart,
+                                                      line = idx,
+                                                      header = L[idx])
+                                    return(res)
+                                  } )
+                                }))
+}
 
 #' Refresh the entire SoilKnowledgeBase inst/extdata/ folder 
 #'
@@ -151,9 +188,11 @@ refresh <- function(...) {
   hed <- SoilKnowledgeBase::parse_nssh_part(dat$part, dat$subpart)
   
   # run inst/scripts/NSSH
-  rpath <- list.files(paste0("inst/scripts/NSSH/", p), ".*.R", full.names = TRUE)
-  res <- lapply(rpath, function(filepath) {
-        if (file.exists(filepath))
-          source(filepath)
-      })
+  for (p in unique(dat$part)) {
+    rpath <- list.files(paste0("inst/scripts/NSSH/", p), ".*.R", full.names = TRUE)
+    res <- lapply(rpath, function(filepath) {
+          if (file.exists(filepath))
+            source(filepath)
+        })
+  }
 }
