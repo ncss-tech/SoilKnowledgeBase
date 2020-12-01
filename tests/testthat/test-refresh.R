149d4c398bc78d2b4ad00a8e1a0163c872f0abac
Andrew G. Brown <andrew.g.brown@usda.gov>
2020-11-30 20:56:33 -0800

    :wrench: Fix README

--- tests/testthat/test-refresh.R
+++ tests/testthat/test-refresh.R
@@ -7,8 +7,9 @@ test_that("refresh (no source PDF download)", {
 })
 
 test_that("refresh (from source; poppler-utils required)", {
-  skip_if_not(suppressWarnings(system('pdftotext')) == 0)
-  res <- refresh()
+  skip_if_not(Sys.info()['sysname'] == 'Linux')
+  skip_if_not(system("dpkg --list | grep poppler-utils") == 0)
+  res <- refresh(keep_pdf = TRUE)
   expect_true(all(unlist(res)))
   unlink("inst", recursive = TRUE)
 })
