88b599ea18648faeb00af14b6d7cdecc2baba1cc
Andrew G. Brown <andrew.g.brown@ca.usda.gov>
2020-12-01 17:22:07 -0800

    :hammer: NSSH 618 parse to JSON

--- R/utils.R
+++ R/utils.R
@@ -8,3 +8,21 @@
 bind_xml_nodeset <- function(xmln) {
   dplyr::bind_rows(lapply(xml2::xml_attrs(xmln), function(x) data.frame(as.list(x))))
 }
+
+#' Convert a named list to JSON
+#' 
+#' @description The main reason for including this wrapper method is to make roxygen aware of jsonlite dependency
+#'
+#' @param a_list An R object to convert to JSON
+#' @param pretty Add whitespace to JSON output? Default: TRUE [argument to \code{jsonlite::toJSON}]
+#' @param auto_unbox Automatically "unbox" vectors of length one? Default: TRUE [argument to \code{jsonlite::toJSON}]
+#' @param ... Additional arguments to \code{jsonlite::toJSON}
+#'
+#' @return A character string formatted as JSON
+#' 
+#' @export
+#'
+#' @importFrom jsonlite toJSON
+convert_to_json <- function(a_list, pretty = TRUE, auto_unbox = TRUE, ...) {
+  jsonlite::toJSON(x = a_list, pretty = pretty, auto_unbox = auto_unbox, ...)
+}
