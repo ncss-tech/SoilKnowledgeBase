bacdfd691eb4d9bb8fd7cf96d9e10616a280a8da
Andrew G. Brown <andrew.g.brown@usda.gov>
2021-03-18 15:33:04 -0700

    create_OSD: Remove ? in pattern for identifying header markers

--- tests/testthat/test-create_OSD.R
+++ tests/testthat/test-create_OSD.R
@@ -3,14 +3,14 @@ test_that("osd_to_json works", {
   set.seed(123)
 
   # sample without replacement -- 1000 OSDs
-  testfiles <- sort(sample(na.omit(list.files("./OSD",
+  testfiles <- na.omit(list.files("OSD",
                              recursive = TRUE,
-                             full.names = TRUE)),
-                      size = 1000))
+                             full.names = TRUE))
 
   # skip if files do not exist
   skip_if_not(length(testfiles) > 0)
 
   # expect they all run without error (does not validate contents)
-  expect_true(all(unlist(osd_to_json(logfile = "test.log", osd_files = testfiles))))
+  expect_true(all(unlist(osd_to_json(logfile = "test.log",
+                                     osd_files = testfiles)))) #sort(sample(testfiles, size = 1000))))))
 })
