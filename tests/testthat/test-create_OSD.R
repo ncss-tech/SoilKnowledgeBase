ae4630a01e090b9ba6ea70e7044e1c1b28351679
Andrew G. Brown <andrew.g.brown@usda.gov>
2021-03-18 14:41:49 -0700

    Beef up osd_to_json test / test output

--- tests/testthat/test-create_OSD.R
+++ tests/testthat/test-create_OSD.R
@@ -1,7 +1,16 @@
 test_that("osd_to_json works", {
-  testfiles <- na.omit(list.files("./OSD/A",
+  # set pseudorandom seed for consistently random results
+  set.seed(123)
+
+  # sample without replacement -- 1000 OSDs
+  testfiles <- sort(sample(na.omit(list.files("./OSD",
                              recursive = TRUE,
-                             full.names = TRUE)[1:100])
+                             full.names = TRUE)),
+                      size = 1000))
+
+  # skip if files do not exist
   skip_if_not(length(testfiles) > 0)
-  expect_true(all(unlist(osd_to_json(osd_files = testfiles))))
+
+  # expect they all run without error (does not validate contents)
+  expect_true(all(unlist(osd_to_json(logfile = "test.log", osd_files = testfiles))))
 })
