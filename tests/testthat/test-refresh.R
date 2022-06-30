01551727db9ed6910a15259f800fd5028776388c
Andrew G. Brown <andrew.g.brown@usda.gov>
2022-06-30 13:31:31 -0700

    foo

--- tests/testthat/test-refresh.R
+++ tests/testthat/test-refresh.R
@@ -1,18 +1,18 @@
 context("Refreshing from external data sources")
 
 test_that("refresh (no source PDF download)", {
-  pdf_path <- file.path(find.package("SoilKnowledgeBase"), "NSSH", "pdf")
+  outpath <- file.path(find.package("SoilKnowledgeBase"), "inst/extdata/")
+  pdf_path <- file.path(outpath, "NSSH/pdf")
   pdfs <- list.files(pdf_path, pattern = "pdf", recursive = TRUE, full.names = TRUE)
   skip_if(length(pdfs) == 0)
-  res <- refresh(download_pdf = FALSE, keep_pdf = TRUE)
+  res <- refresh(outpath = outpath, download_pdf = FALSE, keep_pdf = TRUE)
   expect_true(all(unlist(res)))
   unlink("inst", recursive = TRUE)
 })
 
-test_that("refresh (from source; poppler-utils required)", {
+test_that("refresh (from source; pdftools required)", {
 
-  skip_if_not(Sys.info()['sysname'] == 'Linux')
-  skip_if_not(system("dpkg --list | grep poppler-utils") == 0)
+  skip_if_not_installed("pdftools")
 
   res <- refresh(keep_pdf = TRUE)
   res_test <- unlist(res)
