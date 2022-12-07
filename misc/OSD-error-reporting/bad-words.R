1aeeccaa76f08b026583e3084548c48e9d77b4a9
Beaudette <dylan.beaudette@ca.usda.gov>
2022-12-07 09:45:50 -0800

    case insensitive searching, cleanup, documentation

--- misc/OSD-error-reporting/bad-words.R
+++ misc/OSD-error-reporting/bad-words.R
@@ -1,50 +1,51 @@
+## Search for offensive soil series names in the full text of OSDs. Parsed OSDs via SKB are used for searching.
+## D.E. Beaudette
+## 2022-11-7
+
+
 library(soilDB)
+library(jsonlite)
 library(furrr)
 library(stringi)
 library(pbapply)
-
-
-# bad words
-w <- read.table('E:/temp/badwords.txt')$V1
-
-# series names
-sc <- get_soilseries_from_NASIS()
+library(lattice)
+library(tactile)
 
 # collapse JSON / structured form of OSD -> character vector
 # 1 / series
+# i: series name, case insensitive
 prepareData <- function(i) {
   
+  # local SKB repo
+  # result is an empty data.frame when no matching record is available
   x <- get_OSD(i, base_url = '../../inst/extdata/OSD/')
   
-  # parsed data
+  # remove parsed data
   x$HORIZONS <- NULL
   x$SITE <- NULL
   
+  # flatten sections into single body of text
   x <- paste(unlist(x), collapse = '\n')
   
+  # result is empty character vector when no parsed OSD is available
   return(x)
 }
 
-
-# load from local report in parallel
-# <1 minute from local files
-# 3x time when VPN is in use
-plan(multisession)
-osds <- future_map(sc$soilseriesname, .f = prepareData, .progress = TRUE)
-plan(sequential)
-
-# transfer names
-names(osds) <- sc$soilseriesname
-
-# OK
-cat(osds[[1]])
-
 # search for words in a single OSD text
-findWords <- function(i) {
+# NULL is returned when no matches are found
+# NULL is returned when parsed OSD not available (i is an empty char vector)
+# i: OSD text
+# .words: vector of search terms
+findWords <- function(i, .words) {
   
-  # stringi functions automatically iterate over regex
-  # search for words padded by word boundary markers
-  m <- stri_detect(i, regex = sprintf("\\b%s\\b", w))
+  # stringi functions automatically iterate over regex patterns
+  # using word boundary markers to find entire words, not embedded in others
+  # set case insensitive matching
+  m <- stri_detect_regex(
+    str = i, 
+    pattern = sprintf("\\b%s\\b", .words), 
+    opts_regex = stri_opts_regex(case_insensitive = TRUE)
+  )
   
   # find matches
   idx <- which(m)
@@ -57,49 +58,86 @@ findWords <- function(i) {
   # at least one match found
   
   # this is kind of dumb
-  # extract series name
+  # extract series name from first line of text
   .series <- strsplit(i, '\n', fixed = TRUE)[[1]][1]
   
-  # collapse matches in case > 1
-  .matches <- paste(w[idx], collapse = ', ')
+  # collapse multiple matches
+  .matches <- paste(.words[idx], collapse = ', ')
   
-  # compile result into data.frame
-  res <- data.frame(series = .series, matches = .matches)
+  # compile result: OSD + matching words
+  res <- data.frame(
+    series = .series, 
+    matches = .matches
+  )
   
   return(res)
 }
 
-# ~ 21 seconds fixed string matching (VPN)
+
+## bad words
+w <- read.table('E:/temp/badwords.txt')$V1
+
+# test words: enmsure multiple matches are present
+# w <- c('amador', 'gillender', 'peters', 'pentz')
+
+
+## series names from SC DB via NASIS
+sc <- get_soilseries_from_NASIS()
+
+# series names, as named vector
+# this will translate into named list elements
+s <- sc$soilseriesname
+names(s) <- s
+
+# start parallel back-ends
+plan(multisession)
+
+## load from local GH repo in parallel
+# <1 minute from local files
+# 3x time when VPN is in use
+osds <- future_map(s, .f = prepareData, .progress = TRUE)
+
+# check: OK
+head(names(s))
+cat(osds[[1]])
+
+
+## iterate over list of OSD text, find bad words
+# ~ 21 seconds grep(..., fixed = TRUE) (VPN)
 # ~ 28 minutes REGEX with grep() !
-# ~ 8 seconds REGEX with str_detect()
+# ~ 13 seconds REGEX with str_detect_regex()
+# ~ 5 seconds REGEX with str_detect_regex(), parallel
 # TRUE -> bad word found
-bad <- pblapply(osds, findWords)
+bad <-  future_map(osds, .f = findWords, .words = w, .progress = TRUE)
+
+# stop back-ends
+plan(sequential)
 
 # flatten
 bad <- do.call('rbind', bad)
 row.names(bad) <- NULL
 
-# possible false positives due to location near Squal Valley, CA
+# possible false positives due to location near Squaw Valley, CA
 notbad <- pbsapply(osds, function(i) {
-  m <- stri_detect(str = i, fixed = 'Squaw Valley')
+  m <- stri_detect_fixed(str = i, pattern = 'Squaw Valley')
   m
 })
+
+# 374 series with matches
+nrow(bad)
+
+# frequency of bad words
+dotplot(sort(table(bad$matches)), par.settings = tactile.theme(), xlab = 'Frequency')
 
 # flag as possible false positives
 names(which(notbad))
 
 
-# 326 series with matches
-nrow(bad)
-
-# frequency of bad words
-sort(table(bad$matches), decreasing = TRUE)
-
 # save
 write.csv(bad, file = 'series-with-bad-words.csv', row.names = FALSE)
 
 # describe for Kyle
-knitr::kable(bad[1:10, ])
+knitr::kable(bad[sample(nrow(bad), size = 10), ], row.names = FALSE)
 
 
 
