f74bf4d529701e75aa635a676cebf98f7a0fa3f9
Andrew Gene Brown <andrew.g.brown@usda.gov>
2023-11-29 18:16:37 -0800

    parse_PLANTS: add clean scinames

--- R/create_PLANTS.R
+++ R/create_PLANTS.R
@@ -9,6 +9,7 @@
 create_PLANTS <- function(...) {
   parse_PLANTS(...)
 }
+
 parse_PLANTS <- function(outpath = "inst/extdata", ...) {
 
   x <- data.table::fread('https://plants.usda.gov/assets/docs/CompletePLANTSList/plantlst.txt')
@@ -20,7 +21,18 @@ parse_PLANTS <- function(outpath = "inst/extdata", ...) {
     dir.create(d, showWarnings = FALSE)
   }
 
+  x$sciname <- .clean_PLANT_sciautnames(x$`Scientific Name with Author`)
+
   res <- try(write.csv(x, f, row.names = FALSE,))
 
   !inherits(res, 'try-error') && file.exists(f)
 }
+
+.clean_PLANT_sciautnames <- function(x) {
+  gsub(
+    "^(\u00d7?[A-Z][a-z\\-]+ [\u00d7a-z\\-]+)[^\\.]*( [A-Za-z]+\\.)?( ?[varssp]{3}\\. [a-z\\-]+)?.*$|^([\u00d7?A-Z][a-z\\-]+) [A-Z\\(].*$|(\u00d7?[A-Z][a-z\\-]+) .*|^([A-Z]?[a-z]+-?[A-Z]?[a-z]+ [a-z]+-?[a-z]+) ?.*$|.*",
+    "\\1\\3\\4\\5\\6",
+    x
+  )
+}
+
